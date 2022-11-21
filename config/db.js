b29dfd86a8a0f66fca40ee01a8b04821645e9136
mwegter95 <mwegter95@gmail.com>
2022-11-21 07:22:49 -0600

    added db connection for heroku ClearDB sql database

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b54f28c51b7b1f",
+    password: "c40b497f",
+    database: "heroku_a574ae5dace3e6d",
+})
