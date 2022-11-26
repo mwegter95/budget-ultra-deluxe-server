e34bd40ee004837763139d6a908269dd0028e448
mwegter95 <mwegter95@gmail.com>
2022-11-26 10:01:51 -0600

    added files for egg-js and sequelize setup. Now I need to find out how to serve the sequelize app; what is the start script/process?

---
+++ database/migrations/20221125135900-init-users.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  // The function called when performing a database upgrade, create a `users` table
+  up: async (queryInterface, Sequelize) => {
+    const { INTEGER, DATE, STRING } = Sequelize;
+    await queryInterface.createTable("users", {
+      id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+      name: STRING(30),
+      age: INTEGER,
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  },
+  // The function called when performing a database downgrade, delete the `users` table
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("users");
+  },
+};
